66cc8b80fda783857e84639bbbb0e74c7c75ee7e
Thomas Alex <talex@meradia.com>
2017-06-15 19:19:59 -0400

    LATEST

---
+++ CVB-Web/Controllers/ConsultantController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CVB_Web.Models;
+
+namespace CVB_Web.Controllers
+{
+    public class ConsultantController : Controller
+    {
+        private meradia_db db = new meradia_db();
+
+        // GET: Consultant
+        public ActionResult Index()
+        {
+            var consultants = db.consultants.Include(c => c.title);
+            return View(consultants.ToList());
+        }
+
+        // GET: Consultant/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            consultant consultant = db.consultants.Find(id);
+            if (consultant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(consultant);
+        }
+
+        // GET: Consultant/Create
+        public ActionResult Create()
+        {
+            ViewBag.title_id = new SelectList(db.titles, "ID", "title_nm");
+            return View();
+        }
+
+        // POST: Consultant/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,consultant_nm,title_id,consultant_initials,start_dt,end_dt,image_url,is_admin,is_manager,email_address,noteworthy_intro,birth_dt,headshot,vacation_available")] consultant consultant)
+        {
+            if (ModelState.IsValid)
+            {
+                db.consultants.Add(consultant);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.title_id = new SelectList(db.titles, "ID", "title_nm", consultant.title_id);
+            return View(consultant);
+        }
+
+        // GET: Consultant/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            consultant consultant = db.consultants.Find(id);
+            if (consultant == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.title_id = new SelectList(db.titles, "ID", "title_nm", consultant.title_id);
+            return View(consultant);
+        }
+
+        // POST: Consultant/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,consultant_nm,title_id,consultant_initials,start_dt,end_dt,image_url,is_admin,is_manager,email_address,noteworthy_intro,birth_dt,headshot,vacation_available")] consultant consultant)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(consultant).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.title_id = new SelectList(db.titles, "ID", "title_nm", consultant.title_id);
+            return View(consultant);
+        }
+
+        // GET: Consultant/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            consultant consultant = db.consultants.Find(id);
+            if (consultant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(consultant);
+        }
+
+        // POST: Consultant/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            consultant consultant = db.consultants.Find(id);
+            db.consultants.Remove(consultant);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- CVB-Web/Models/ModelCvbDataAnnotations.cs
+++ CVB-Web/Models/ModelCvbDataAnnotations.cs
@@ -1,7 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace CVB_Web.Models
 {
+    // --------------- app_domain -------------------------------------------
     // apply metadata from this class...
     public class app_domain_MetaData {
         [Display(Name = "Name")]
@@ -10,12 +13,24 @@ namespace CVB_Web.Models
     // to this class...
     [MetadataType(typeof(app_domain_MetaData))] public partial class app_domain { }
 
+    // --------------- app_vendor -------------------------------------------
     public class app_vendor_MetaData {
         [Display(Name = "Name")]
         public string app_vendor_nm;
     }
     [MetadataType(typeof(app_vendor_MetaData))] public partial class app_vendor { }
 
+    // --------------- title -------------------------------------------
+    public class title_MetaData
+    {
+        [Display(Name = "Title")]
+        public string title_nm;
+        [Display(Name = "Description")]
+        public string title_descr;
+    }
+    [MetadataType(typeof(title_MetaData))] public partial class title { }
+    
+    // --------------- company -------------------------------------------
     public class company_MetaData {
         [Display(Name = "Name")]
         public string company_nm;
@@ -29,4 +44,48 @@ namespace CVB_Web.Models
         public string company_short_nm;
     }
     [MetadataType(typeof(company_MetaData))] public partial class company { }
+
+    // --------------- consultant -------------------------------------------
+    public class consultant_MetaData
+    {
+        [Display(Name = "Name")]
+        public string consultant_nm;
+
+        [Display(Name = "Title")]
+        public int title_id;
+
+        [Display(Name = "Initials")]
+        public string consultant_initials;
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Start Date")]
+        [DisplayFormat(DataFormatString = "{0:MMM dd, yyyy}")]
+        public Nullable<System.DateTime> start_dt;
+
+        [DataType(DataType.Date)]
+        [Display(Name = "End Date")]
+        [DisplayFormat(DataFormatString = "{0:MMM dd, yyyy}")]
+        public Nullable<System.DateTime> end_dt;
+
+        [Display(Name = "Is Admin")]
+        public bool is_admin;
+
+        [Display(Name = "Is Manager")]
+        public bool is_manager;
+        [Display(Name = "Email")]
+        public string email_address;
+        [Display(Name = "Noteworthy Intro")]
+        public string noteworthy_intro;
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Birthday")]
+        [DisplayFormat(DataFormatString = "{0:MMM dd, yyyy}")]
+        public Nullable<System.DateTime> birth_dt;
+
+        [Display(Name = "Vacation Days")]
+        public Nullable<decimal> vacation_available;
+    }
+    [MetadataType(typeof(consultant_MetaData))] public partial class consultant { }
+
+
 }
